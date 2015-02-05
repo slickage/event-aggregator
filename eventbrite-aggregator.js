d7696aad9d7599659b66d67406f8f4b4bd71c20d
Trevor Alexander <talex@privatdemail.net>
2015-02-05 13:29:21 -1000

    added basic eventbrite query func, npm dependencies

---
+++ eventbrite-aggregator.js
@@ -0,0 +1,45 @@
+// Eventbrite event searching
+// Anonymous query:
+// https://www.eventbriteapi.com/v3/events/search/?token=5OTKXGRDYWFRA2SWONXT
+// API doc:
+// https://developer.eventbrite.com/docs/event-search/
+var http = require('http');
+var _ = require('lodash');
+
+var baseURL = 'https://www.eventbriteapi.com/v3/events/search/';
+var token = '5OTKXGRDYWFRA2SWONXT';
+
+var getEvents = function(url, authToken, queryHash) {
+  // function expects a base url, a token for the request auth, and a hash of
+  // query terms with their values
+  var vals = Object.keys(queryHash).map(function(thiskey) {
+    return(queryHash[thiskey]);
+  });
+  
+  var queryPairs = _.zip(Object.keys(queryHash), vals);
+  var queryString = queryPairs.map(function(x) { 
+    return(x[0] + '=' + x[1])
+  }).reduce(function(y,z) { 
+    return(y + '&' + z);
+  });
+
+  var options = {
+    host: baseURL,
+    path: queryString;
+  };
+
+  callback = function(response) {
+    var str = '';
+
+    // for now just collect up all the data and return the whole thing as string
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+    response.on('end', function () {
+      return(str);
+    });
+  }
+
+  http.request(options, callback).end();
+
+}
