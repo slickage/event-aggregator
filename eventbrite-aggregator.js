6c7ba7295df3faf08095d7057a19929343791163
Trevor Alexander <talex@privatdemail.net>
2015-02-05 19:21:47 -1000

    broke query parsing/conversion out to own function

--- eventbrite-aggregator.js
+++ eventbrite-aggregator.js
@@ -3,34 +3,24 @@
 // https://www.eventbriteapi.com/v3/events/search/?token=5OTKXGRDYWFRA2SWONXT
 // API doc:
 // https://developer.eventbrite.com/docs/event-search/
-var http = require('http');
+var request = require('request');
 var _ = require('lodash');
 
 // TODO move these out
-var baseURL = 'eventbriteapi.com';
 var token = '5OTKXGRDYWFRA2SWONXT';
 
 var getEventbriteEvents = function(authToken, queryHash) {
   // function expects a base url, a token for the request auth, and a hash of
   // query terms with their values
 	if (typeof(queryHash) !== 'undefined') { // if query hash passed
-		var vals = Object.keys(queryHash).map(function(thiskey) {
-			return(queryHash[thiskey]);
-		});
-		
-		var queryPairs = _.zip(Object.keys(queryHash), vals);
-		var queryString = queryPairs.map(function(x) { 
-			return(x[0] + '=' + x[1]);
-		}).reduce(function(y,z) { 
-			return(y + '&' + z);
-		});
+		var queryString = hashToGET(queryHash);
 	} else { // if no query hash passed
 		var queryString = '';
 	}
 
   var options = {
-    hostname: 'www.eventbriteapi.com',
-    path: '/v3/events/search/?' + queryString + '&token=' + token,
+    hostname: 'https://www.eventbriteapi.com'+'/v3/events/search/?' +
+			queryString + '&token=' + token,
 		method: 'GET'
   };
 
@@ -57,6 +47,24 @@ var getEventbriteEvents = function(authToken, queryHash) {
 	});
 
 	getReq.end();
+}
+
+var hashToGET = function(queryHash) {
+	// converts a set of key-value pairs to a string using the typical GET
+	// format of key=value&key=value&...
+
+	// get values as array
+	var vals = Object.keys(queryHash).map(function(thiskey) {
+		return(queryHash[thiskey]);
+	});
+	// interleave keys and values
+	var queryPairs = _.zip(Object.keys(queryHash), vals);
+	// join as key=value string
+	var queryString = queryPairs.map(function(x) { 
+		return(x[0] + '=' + x[1]);
+	}).reduce(function(y,z) { 
+		return(y + '&' + z);
+	});
 }
 
 exports['getEventbriteEvents'] = getEventbriteEvents;
