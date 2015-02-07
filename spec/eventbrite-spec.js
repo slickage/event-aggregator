f9d79f465b7b62ddb96bd10c708949e49c3f2c52
Trevor Alexander <talex@privatdemail.net>
2015-02-06 17:35:29 -1000

    fixed eventbrite async jasmine tests
    - switched library require to https

--- spec/eventbrite-spec.js
+++ spec/eventbrite-spec.js
@@ -1,26 +1,49 @@
 describe('eventbrite event fetcher', function() {
-	var http = require('http');
+	var https = require('https');
 	var eb = require('../eventbrite-aggregator.js');
 
-	var token = '5OTKXGRDYWFRA2SWONXT';
+ 	var token = '5OTKXGRDYWFRA2SWONXT';
 	var queryHash = '{}';
+
+	beforeEach(function() { // change timeout interval
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  });
 	
 	it('exports a query function', function() {
 		expect(typeof(eb.getEventbriteEvents)).toBe('function');
 	});
 
 	it('submits an HTML request', function() {
-		spyOn(http, 'request').and.callThrough();
+		spyOn(https, 'request').and.callThrough();
 
 		eb.getEventbriteEvents(token);
 
-		expect(http.request).toHaveBeenCalled();
+		expect(https.request).toHaveBeenCalled();
 	});
 
-	it('returns a JSON string of valid events', function() {
-		var queryReturn = eb.getEventbriteEvents(token);
-		expect(typeof(queryReturn) === 'defined');
-		expect(queryReturn.hasOwnProperty('events'));
-	});
+	it('returns a JSON string of valid events', function(done) {
+		callback = function(response) { // function for async data return
+			var str = '';
+			
+			// console.log('HTTP', response.statusCode);
+			// console.log('Request header: ', JSON.stringify(response.headers));
+			// for now just collect up all the data and return the whole thing as string
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+			response.on('end', function () {
+				var queryReturn = JSON.parse(str);
 
+				expect(typeof(queryReturn) === 'defined');
+				expect(Array.isArray(queryReturn.events));
+				done();
+			});
+		}
+		eb.getEventbriteEvents(token, callback);
+
+		afterEach(function() { // restore timeout interval
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+	});
 });
