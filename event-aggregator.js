897e63b7ebd9b03f3a8dd9bef59b09dac14b5eb6
Trevor Alexander <talex@privatdemail.net>
2015-02-18 15:48:01 -1000

    skeleton for main event aggregator

--- event-aggregator.js
+++ event-aggregator.js
@@ -1,9 +1,10 @@
 // main aggregation function
-
+var https = require('https');
 var fs = require('fs');
-var agg = require('./eventprovidermodules.js');
+var agg = require('./eventprovidermodules.js'); // query providers
 
-var eventAggregator = function() {
+var eventAggregator = function(queryHash) {
+	// bring in config vars
 	try {
 		var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 	} catch(err) {
@@ -13,6 +14,49 @@ var eventAggregator = function() {
 			throw err;
 		}
 	}
+
+	
+	
+	// call each of the query providers brought in with matching configs
+	Object.keys(agg).map(function(thisQueryFunc) {
+		console.log(agg[thisQueryFunc]);
+		agg[thisQueryFunc](config[thisQueryFunc]['token'],
+												submitEvents, queryHash);
+	});
 }
+
+// functions for digesting and simplifying event provider returns so they meet
+// spec
+var simplifyEventbriteEvents = function() {
+
+};
+
+var simplifyMeetupEvents = function() {
+
+};
+
+// our callback for sending event results through the hnl.io API
+var submitEvents = function(eventObj) {
+
+	// simplify events
+	// TODO make more general, simple
+	switch(submitEvents.caller.name) {
+		// we know how to simplify because we know which provider the callback was
+		// passed to
+	case 'getEventbriteEvents' :
+		var eventList = simplifyEventbriteEvents(eventObj);
+		break;
+
+	case 'getMeetupEvents' :
+		var eventList = simplifyMeetupEvents(eventObj);
+		break;
+		
+	}
+
+	// build up POST request, submit simplified event objects one by one
+	for (thisEvent in eventList) {
+		
+	}
+};
 
 module.exports = eventAggregator;
