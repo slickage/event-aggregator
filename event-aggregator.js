9ac775ed1732eba69f38b6d18a646377582d9726
Trevor Alexander <talex@privatdemail.net>
2015-02-20 10:16:16 -1000

    improved organization of main aggregator func

--- event-aggregator.js
+++ event-aggregator.js
@@ -15,45 +15,52 @@ var eventAggregator = function(queryHash) {
 		}
 	}
 
-	
-	
 	// call each of the query providers brought in with matching configs
 	Object.keys(agg).map(function(thisQueryFunc) {
-		console.log(agg[thisQueryFunc]);
-		agg[thisQueryFunc](config[thisQueryFunc]['token'],
-												submitEvents, queryHash);
+    // console.log('func: ', thisQueryFunc);
+    // console.log('token: ', config['providers'][thisQueryFunc]['token']);
+		agg[thisQueryFunc](config['providers'][thisQueryFunc]['token'],
+											 submitEvents, queryHash);
 	});
 }
 
-// functions for digesting and simplifying event provider returns so they meet
-// spec
-var simplifyEventbriteEvents = function() {
 
-};
-
-var simplifyMeetupEvents = function() {
-
-};
 
 // our callback for sending event results through the hnl.io API
 var submitEvents = function(eventObj) {
 
 	// simplify events
-	// TODO make more general, simple
+	// TODO make more general, simple (hash)
 	switch(submitEvents.caller.name) {
 		// we know how to simplify because we know which provider the callback was
 		// passed to
 	case 'getEventbriteEvents' :
-		var eventList = simplifyEventbriteEvents(eventObj);
+		var eventList = submitEventbriteEvents(eventObj);
 		break;
 
 	case 'getMeetupEvents' :
-		var eventList = simplifyMeetupEvents(eventObj);
+		var eventList = submitMeetupEvents(eventObj);
 		break;
 		
-	}
+  }
 
-	// build up POST request, submit simplified event objects one by one
+  // send the event list to hnl.io
+  POSTEvents(eventList, config.api_url);
+};
+
+// functions for simplifying event provider returns so they meet spec and then
+// passing them on to POSTEvent, one by one
+var submitEventbriteEvents = function() {
+  
+};
+
+var submitMeetupEvents = function() {
+
+};
+
+var POSTEvents = function(eventList, apiURL) {
+
+  // build up POST request, submit simplified event objects one by one
 	for (thisEvent in eventList) {
 		
 	}
