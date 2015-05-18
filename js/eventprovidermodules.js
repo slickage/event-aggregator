9ca55e03b00eb55ee690ac701be74b9261949296
Trevor Alexander <talex@privatdemail.net>
2015-05-17 15:44:20 -1000

    added array gcal aggregator to event aggregator providers manifest

--- js/eventprovidermodules.js
+++ js/eventprovidermodules.js
@@ -1,5 +1,6 @@
 // simple manifest of exportable functions for this component
 module.exports = {
 	getEventbriteEvents : require('./eventbrite-aggregator.js'),
-	getMeetupEvents : require('./meetup-aggregator.js')
+	getMeetupEvents : require('./meetup-aggregator.js'),
+  getAllGCalEvents : require('./gcal-aggregator.js')
 };
