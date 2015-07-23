edc1eb190bcb1389ce1a2a09210a168d5359e2e1
Trevor Alexander <talex@privatdemail.net>
2015-07-23 12:41:53 -1000

    initial rss scraper

---
+++ js/rss-aggregator.js
@@ -0,0 +1,31 @@
+var FeedParser = require('feedparser')
+var request = require('request');
+
+var req = request('http://www.techhui.com/events/event/feed?xn_auth=no')
+var feedparser = new FeedParser();
+
+req.on('error', function (error) {
+  // handle any request errors
+});
+req.on('response', function (res) {
+  var stream = this;
+
+  if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+
+  stream.pipe(feedparser);
+});
+
+
+feedparser.on('error', function(error) {
+  // always handle errors
+});
+feedparser.on('readable', function() {
+  // This is where the action is!
+  var stream = this
+    , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+    , item;
+
+  while (item = stream.read()) {
+    console.log(item);
+  }
+});
