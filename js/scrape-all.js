e86248eb4a62845ff9add5f69c8e475a835632cc
Trevor Alexander <talex@privatdemail.net>
2015-07-28 22:24:41 -1000

    minor edits

--- js/scrape-all.js
+++ js/scrape-all.js
@@ -10,10 +10,10 @@ var twoWeeksLater = new Date();
 twoWeeksLater = twoWeeksLater.setDate(twoWeeksLater.getDate() + 14);
 
 var query = { // honolulu airport
-	'lat' : 21.33,
-	'lon' : -157.94,
-	'radius' : 1000000, // 1000km
-	'time_end' : twoWeeksLater.valueOf(),
+  'lat' : 21.33,
+  'lon' : -157.94,
+  'radius' : 1000000, // 1000km
+  'time_end' : twoWeeksLater.valueOf(),
   'keywords' : config.keywords
 };
 
